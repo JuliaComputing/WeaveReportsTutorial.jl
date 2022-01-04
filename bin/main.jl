e5edaeba6234a00ccf8a5f29cffc5091f6633057
jeremie.db <jeremie.db@evovest.com>
2022-01-03 20:45:18 -0500

    initial commit

--- bin/main.jl
+++ bin/main.jl
@@ -2,9 +2,6 @@ using Weave
 using JSON3
 using Tar
 
-# results file returns the html (default) - otherwise zip together the html and figures
-const output = get(ENV, "output", "html")
-
 const xaxis = get(ENV, "xaxis", "sepal.length")
 const yaxis = get(ENV, "yaxis", "petal.width")
 const date = Dates.today()
@@ -18,11 +15,4 @@ weave("$(@__DIR__)/../reports/$(report_name).jmd",
     doctype = "md2html",
     args = (date = date, xaxis = xaxis, yaxis = yaxis))
 
-ENV["RESULTS"] = JSON3.write(Dict("results directory" => readdir(path_results)))
-
-if output == "html"
-    ENV["RESULTS_FILE"] = "$(path_results)/$(report_name).html"
-else
-    tarball = Tar.create(path_results)
-    ENV["RESULTS_FILE"] = tarball
-end
+ENV["RESULTS_FILE"] = "$(path_results)/$(report_name).html"
